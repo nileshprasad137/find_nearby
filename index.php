dc26dd92ae76257e220d04e33590cb6c7c56808c
Nilesh Prasad <prasadnilesh96@gmail.com>
2016-10-27 18:33:11 +0530

    navbar chnaged of index.php

--- index.php
+++ index.php
@@ -80,31 +80,11 @@ session_start();
  <body>
   
   <nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#"></a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active "><a href="#">Home</a></li>  
-           
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <!--<li class="active "><a href="signup.php"><span class="glyphicon glyphicon-user"></span> SignUp</a></li>
-        <li class="active "><a href="#myModal" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-log-in" ></span> Login</a></li>
-        -->
-        <li class="active "><a href="about.php"><span class="glyphicon glyphicon-menu-hamburger"></span> About</a></li>  
-            
-
-      </ul>
-    </div>
-  </div>
-</nav>
+  
+      <a class="navbar-brand" href="#" >Home</a>
+      <a class="navbar-brand navbar-right" href="about.php"  >About</a>
+    
+  </nav>
 
 
 <div class="jumbotron">
